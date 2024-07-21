75985e057861849a3cf6ce595468d35834ac8ed6
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-21 20:32:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import cvzone
 import numpy as np
+cap = cv2.VideoCapture(0)  # Change to '0' to use the default webcam
+cap.set(3, 1280)  # Width of the frame
+cap.set(4, 720)   # Height of the frame
+detector = HandDetector(detectionCon=0.8)  # Hand detection confidence
+colorR = (255, 0, 255)  # Color for rectangle (Pink)
