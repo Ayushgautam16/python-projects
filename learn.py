4bd1cdfd18091408d918bd8fb548c2421a808fb3
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-03 01:03:24 +0530

    1

---
+++ learn.py
@@ -0,0 +1,187 @@
+# # why python 3 
+# # 3.13.2 is the current version
+# # ​As of April 2, 2025, the latest stable release of Python is version 3.13.2, released on February 4, 2025. ​
+# # Python.org
+# # +1
+# # Python.org
+# # +1
+
+# print('this is the example of pop pop')
+# # u can use the double and single quotes aswell
+# print("this is the example  'hi'")
+
+
+# print('this \' is the example  ')
+
+# print('hi '+'there')
+
+# print('hi '+str(5))
+
+# print(int(5)+ 7)
+
+# print(int('5')+ 7)
+
+# # hi there
+# # hi 5
+# # 12
+# # 12
+
+# print(float('5.6')+9)
+# # 14.6
+
+# print(int(5/2))
+
+# examplevar =  3444
+
+# print(examplevar)
+
+
+# condition =1
+
+# while condition<10:
+#     print(condition)
+#     condition +=1
+
+# while True:
+#     print('doing stuff')
+#     this is the infinite loop
+
+
+# if statements
+
+# x = 5
+# y=8
+
+# if x>y:
+#     print("chhota hai number")
+# else:
+#     print("bada number hai bhau")
+# if x<y:
+#     print("chhota hai number")
+# else:
+#     print("bada number hai bhau")
+
+# ef elif 
+
+# x = 5
+# y = 4
+# z = 18
+# if x<y:
+#     print("x is this ")
+# elif x>y:
+#     print  ("x bada hai y se")
+# elif(y<Z):
+#     print("z bada hai y se")
+# else:
+#     print("z bada hai y se")
+
+# function:
+
+# def example_of_function():
+#     print("basic function")
+#     z =3+5
+#     print(z)
+# example_of_function()
+
+
+# def simp_addition(num1, num2):
+#     answer = num1+num2
+#     print(answer)
+
+# simp_addition(3,6)
+# 9
+
+# default parameters
+
+
+# def simple(num1,num2):
+#     pass
+# def simple(num1,num2=5):
+#     print(num1,num2)
+
+# def basic_window(width,height,font='tnr',bgc='w',scrollbar=True):
+#     print(width,height,bgc, font )
+
+# basic_window(500,350)
+
+
+# global and local variable
+
+# x =5
+
+# def example():
+#     z1=5
+#     global x 
+#     print(x)
+#     print(x+4)
+
+#     x+=6
+
+#  print(x)
+#           ^
+# UnboundLocalError: cannot access local variable 'x' where it is not associated with a value
+
+
+
+
+# suppose if we dont wanted to use the global variable 
+# so we can assign the x variable to local variable inside the function
+
+# globx =x
+# print(globx)
+# globx+=5
+
+# x = example()
+# print(x)
+
+
+# there is no concept of the constant in the python that can be change in the run time 
+
+# set is the unorder list 
+# it cannot contains the duplicate ValueError
+# {""''}
+
+
+# dictionary is the key value pair 
+
+
+# ther is char data type in python
+
+# a=22447
+# b=2
+# print(a//b)
+
+# relational operator
+# <,<=,
+# Operator	Description	Example	Output
+# ==	Equal to	5 == 5	True
+# !=	Not equal to	5 != 3	True
+# >	Greater than	7 > 3	True
+# <	Less than	4 < 6	True
+# >=	Greater than or equal to	8 >= 8	True
+# <=
+
+# # logical 
+# and/ or/ not
+
+# Assignment operator
+
+# Operator	Description	Example	Equivalent To
+# =	Assign value	a = 10	a = 10
+# +=	Add and assign	a += 5	a = a + 5
+# -=	Subtract and assign	a -= 3	a = a - 3
+# *=	Multiply and assign	a *= 2	a = a * 2
+# /=	Divide and assign	a /= 4	a = a / 4
+# //=	Floor divide and assign	a //= 3	a = a // 3
+# %=	Modulus and assign	a %= 7	a = a % 7
+# **=	Exponentiation and assign	a **= 2	a = a ** 2
+# &=	Bitwise AND and assign	a &= 1	a = a & 1
+# `	=`	Bitwise OR and assign	`a
+# ^=	Bitwise XOR and assign	a ^= 3	a = a ^ 3
+# >>=	Right shift and assign	a >>= 2	a = a >> 2
+# <<=
+
+# bitwise operator
+# ^,|,&&,<<,>>,~
+
+# membership operator 
